59e42a3c0cbf18978cfcb5482e8c9ec6d303621a
rajkumarmotivationspeaker <162527273+rajkumarmotivationspeaker@users.noreply.github.com>
2025-08-30 00:52:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Highlight current navigation item
 const navLinks = document.querySelectorAll('nav ul li a');
 navLinks.forEach(link => {
-  link.addEventListener('click', function() {
+  link.addEventListener('click', function () {
     navLinks.forEach(l => l.classList.remove('active'));
     this.classList.add('active');
   });
@@ -15,10 +15,11 @@ navLinks.forEach(link => {
 
 
 // Welcome messager 
-function stp(){+
-alert('Welcome to the Hospital Management System!');
-}  
-setTimeout(stp,4000);
+function stp() {
+  +
+  alert('Welcome to the Hospital Management System!');
+}
+setTimeout(stp, 4000);
 
 
 // let wlcm = document.querySelector(".wlc_img")
@@ -28,3 +29,38 @@ setTimeout(stp,4000);
 // }
 
 // setTimeout(stp,2000);
+
+
+
+
+// menu icon functionality starts here
+
+
+let menuIcon_value = document.querySelector('.menu-icon1').className;   // acces the class name
+let menuIcon1 = document.querySelector('.menu-icon1')
+let menuIcon2 = document.querySelector('.menu-icon2')
+menuIcon2.style.display = "none";
+let sub_menu = document.querySelector('.sub_menu');
+
+function stp() {
+  sub_menu.style.display = "block";
+
+  if (menuIcon_value == "fa-solid fa-bars menu-icon menu-icon1") {
+    // alert("hello if entered");
+    menuIcon1.style.display = "none";
+    menuIcon2.style.display = "block";
+   
+  }
+}
+
+function stpclose() {
+  sub_menu.style.display = "none";
+
+  if (menuIcon_value == "fa-solid fa-bars menu-icon menu-icon1") {
+    menuIcon1.style.display = "block";
+    menuIcon2.style.display = "none";
+  }
+}
+
+// menu icon functionality ends here
+
